b67ea49e7034d32be5b24525978e5f5904e6c367
Blake <blake@academiki.com>
2021-05-31 17:48:20 -0400

    Init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import {Button,TextField,createMuiTheme,ThemeProvider,CssBaseline} from "@material-ui/core"
+import React from "react";
+import firebase from "firebase";
 
 function App() {
+  const emailInput = React.createRef()
+  const passwordInput = React.createRef()
+  
+  const firebaseConfig = {
+    apiKey: "AIzaSyCPA0RTcpqqbP5fGRxNFM4qijUEzpvWhrw",
+    authDomain: "todolist-a9da8.firebaseapp.com",
+    projectId: "todolist-a9da8",
+    storageBucket: "todolist-a9da8.appspot.com",
+    messagingSenderId: "1062884920911",
+    appId: "1:1062884920911:web:5bdb903e6ec2ee1bd2a43a",
+    measurementId: "G-9T1X7W0FTD"
+  };
+
+  const darkTheme = createMuiTheme({
+    palette: {
+      type: 'dark',
+    },
+  });
+
+  function login() {
+    const email = emailInput.current.value
+    const password = passwordInput.current.value
+    firebase.auth().signInWithEmailAndPassword(email, password)
+    .then((userCredential) => {
+      var user = userCredential.user
+      console.log("Logged In")
+    })
+    .catch((error) => {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      console.log("Error")
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={darkTheme}>
+    <CssBaseline/>
+    <form onSubmit={login}>
+      <TextField inputRef={emailInput} id="outlined-basic" label="Username" variant="outlined" />
+      <TextField inputRef={passwordInput} id="outlined-basic" label="Password" type="password" variant="outlined" />
+      <Button onClick={login} variant="contained" color="primary">Login</Button>
+    </form>
+    </ThemeProvider>
   );
 }
 
